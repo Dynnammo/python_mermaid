c97f8c5c32ae8132579b3026975bd38a91ce5346
THIVEND <baptiste.thivend@protonmail.com>
2022-12-21 11:28:24 +0100

    Move Noce in dedicated file

---
+++ python_mermaid/node.py
@@ -0,0 +1,32 @@
+# Shapes are created following the documentation here :
+# https://mermaid.js.org/syntax/flowchart.html#node-shapes
+NODE_SHAPES = {
+    "normal": {"start":"[","end":"]"},
+    "round-edge": {"start":"(","end":")"},
+    "stadium-shape": {"start":"([","end":"])"},
+    "subroutine-shape": {"start":"[[","end":"]]"},
+    "cylindrical": {"start":"[(","end":")]"},
+    "circle": {"start":"((","end":"))"},
+    "label-shape": {"start":">","end":"]"},
+    "rhombus": {"start":"{","end":"}"},
+    "hexagon": {"start":"{{","end":"}}"},
+    "parallelogram": {"start":"[/","end":"/]"},
+    "parallelogram-alt": {"start":"[\\","end":"\\]"},
+    "trapezoid": {"start":"[/","end":"\\]"},
+    "trapezoid-alt": {"start":"[\\","end":"/]"},
+    "double-circle": {"start":"(((","end":")))"},
+}
+
+class Node:
+    def __init__(self, id: str, content: str = None, shape: str = "normal"):
+        self.id = id
+        self.content = content if content else id
+        self.shape = NODE_SHAPES[shape]
+        
+        if shape not in NODE_SHAPES.keys():
+            raise ValueError(f"Shape given is not a normalized shape: {shape}. Please try one of the following: {str(NODE_SHAPES.keys())}")
+        
+        # TODO: verify that content match a working string pattern
+
+    def __str__(self):
+        return f"{self.id}{self.shape['start']}\"{self.content}\"{self.shape['end']}"
