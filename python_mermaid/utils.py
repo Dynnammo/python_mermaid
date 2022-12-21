a0c5d9b80db28e300c9686e0c2b93c0d2a4e9021
Dynnammo <33259633+Dynnammo@users.noreply.github.com>
2022-12-21 22:34:56 +0100

    Asciify IDs of nodes (#14)
    * Asciify IDs of nodes
    
    * Add unidecode in dependencies

--- python_mermaid/utils.py
+++ python_mermaid/utils.py
@@ -1,10 +1,14 @@
 from re import sub
+from unidecode import unidecode
 
 
 def snake_case(s):
-    return '_'.join(
+    s = unidecode(s)
+    s = '_'.join(
         sub(
             '([A-Z][a-z]+)', r' \1',
             sub('([A-Z]+)', r' \1', s.replace('-', ' '))
         ).split()
     ).lower()
+
+    return s
