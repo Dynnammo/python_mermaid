de7597e6c7986c444d2e97cc1d002808766cdce6
THIVEND <baptiste.thivend@protonmail.com>
2022-12-21 14:17:55 +0100

    Adapt to new line types

--- tests/test_link.py
+++ tests/test_link.py
@@ -3,9 +3,10 @@ from python_mermaid.link import Link
 from .utils import LINK_1, LINK_2
 
 def test_link_without_message():
-    assert str(LINK_1) == f"{LINK_1.origin.id} {LINK_1.shape} {LINK_1.end.id}"
+    l = LINK_1
+    assert str(LINK_1) == f"{l.origin.id} {l.head_left}{LINK_1.shape}{l.head_right} {LINK_1.end.id}"
 
 def test_link_with_message():
     l = LINK_1
     l.message = "This is a message"
-    assert str(l) == f"{l.origin.id} {l.shape} |{l.message}| {l.end.id}"
+    assert str(l) == f"{l.origin.id} {l.head_left}{l.shape}{l.head_right}|{l.message}| {l.end.id}"
