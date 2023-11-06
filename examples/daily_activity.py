f3356f7353291c1c278a1cc4a37354cc497e7243
nos86 <salvo.nos86@gmail.com>
2023-11-06 16:53:55 +0100

    add example for state diagram

---
+++ examples/daily_activity.py
@@ -0,0 +1,37 @@
+from python_mermaid.diagram import MermaidDiagram
+from python_mermaid.node import StateNode
+from python_mermaid.link import StateLink
+
+sleep = StateNode("Sleep")
+breakfast = StateNode("Eat breakfast")
+work = StateNode("Go to work")
+fun = StateNode("Have fun")
+lunch = StateNode("Eat lunch")
+work_again = StateNode("Work again")
+visit_parents = StateNode("Visit parents")
+dinner = StateNode("Eat dinner")
+
+m = MermaidDiagram(type="statechart",
+                   nodes = [sleep, 
+                            breakfast, 
+                            work, 
+                            fun, 
+                            lunch, 
+                            work_again, 
+                            visit_parents, 
+                            dinner],
+                   links =  [
+                       StateLink(sleep, breakfast),
+                       StateLink(breakfast, work, "Working day"),
+                       StateLink(work, lunch, "Have time for lunch"),
+                       StateLink(work, work_again, "No time for lunch"),
+                       StateLink(work_again, dinner),
+                       StateLink(dinner, sleep),
+                       StateLink(breakfast, fun, "Weekend"),
+                       StateLink(fun, lunch),
+                       StateLink(lunch, visit_parents),
+                       StateLink(visit_parents, dinner)
+                   ])
+
+m.add_start_and_end_nodes(breakfast, None)
+print(m)
